d2c5858485a6bc1345044151bf615ba345a3f907
Madhav Jha <madhavjha@gmail.com>
2014-09-28 15:16:12 -0700

    basic server

--- server.go
+++ server.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	s := r.URL.Path[1:]
+	fmt.Fprintf(w, "Hi there, I love %s!", s)
+}
+ 
+
 func main() {
-	fmt.Println("Hi")
+	http.HandleFunc("/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
